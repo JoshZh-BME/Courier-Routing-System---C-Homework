509d51169512239891340eff15d83f6e73ff6aec
JoshuaZh1002 <1465481117zxy@gmail.com>
2026-05-05 16:58:32 +0000

    Macro added

---
+++ include/macro.h
@@ -0,0 +1,12 @@
+#ifndef MACRO_H
+#define MACRO_H
+
+#include <limits.h>
+
+// --- Konstansok, Alapértékek beállitása demo teszt esethez --- //
+#define MAX_LOCATION 100
+#define MAX_NAME_LEN 64
+#define INF INT_MAX
+#define MAX_STOPS 50
+
+#endif
